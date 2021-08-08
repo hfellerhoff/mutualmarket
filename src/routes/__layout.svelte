936edb2b73eb9dbbc88bf6070fc5cb63a83b970d
Henry Fellerhoff <henryfellerhoff@gmail.com>
2021-08-08 18:42:31 -0400

    feat: landing page

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<slot />
+
+<style>
+	:global(*) {
+		box-sizing: border-box;
+	}
+	:global(body, html) {
+		background-color: #ffede7;
+		padding: 0;
+		margin: 0;
+	}
+	:global(h1, h2, h3, h4, h5, h6) {
+		font-family: 'Rammetto One';
+	}
+	:global(p, span) {
+		font-family: 'Josefin Sans';
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<div id="logo">
+		<h1>mutual market</h1>
+		<p>Supporting local communities everyday purchases you already make. Coming soon.</p>
+	</div>
+</main>
+
+<style>
+	main {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		padding: 2rem;
+	}
+	#logo {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		max-width: 24rem;
+	}
+	#logo h1 {
+		line-height: 1.5rem;
+	}
+	h1 {
+		margin: 0;
+		padding: 0;
+	}
+	#logo p {
+		font-size: 1.2rem;
+		line-height: 1.5rem;
+	}
+</style>
